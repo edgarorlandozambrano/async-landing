1703cf04b0f1534ff188d5d8a3280624c2dbeb23
Edgar Zambrano <edgarorlandozambrano@gmail.com>
2023-07-29 10:39:39 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,40 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCif1iBrfp0KbYi2WiD5KMEg&part=snippet%2Cid&order=date&maxResults=5';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '601006cd68msh2904b34e189d603p1e24fdjsn75aeec5733b0',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi,options)
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+        <div class="group relative">
+            <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+            </div>
+            <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${video.snippet.title}
+                </h3>
+            </div>
+        </div>
+        `).slice(0,4).join('')}
+            
+        `;
+        content.innerHTML = view;
+    } catch(error){
+        console.log(error);
+    }
+}) ();
